8d7bf1428e3cac0ffbebfd38bb7713b2d9b70fc5
Chris Murphy <_chrism_@live.com>
2018-04-17 22:20:54 +0100

    makes changes to app.js

--- client/src/app.js
+++ client/src/app.js
@@ -1,7 +1,7 @@
-const QuoteView = require('./views/quoteView');
+const QuoteView = require('./views/reviewView');
 const Request = require('./services/request.js');
 
-const quoteView = new QuoteView();
+const quoteView = new ReviewView();
 const request = new Request('http://localhost:3000/api/reviews');
 
 const appStart = function(){
