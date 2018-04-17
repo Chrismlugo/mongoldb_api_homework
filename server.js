8a8dabc6aff480a1bafe4b40cfa01e3e7b481b85
Chris Murphy <_chrism_@live.com>
2018-04-17 21:19:07 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const parser = require('body-parser');
+const server = express();
+
+server.use(parser.json());
+server.use(express.static('client/build'));
+server.use(parser.urlencoded({extended: true}));
+
+const MongoClient = require('mongodb').MongoClient
+
+MongoClient.connect('mongodb://localhost:27017', function(err, client) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  const db = client.db("star_wars");
+
+  console.log('Connected to database');
+
+server.listen(3000, function(){
+  console.log("Listening on port 3000");
+});
